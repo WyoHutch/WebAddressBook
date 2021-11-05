f94979c3794a4e09fdb7bdfd33923a22ebf471ce
Glenn Hutchinson <ghutchinson@digis.net>
2021-11-05 13:59:33 -0600

    Added User Logins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_033753) do
+ActiveRecord::Schema.define(version: 2021_11_05_192031) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "last_name"
@@ -22,6 +22,30 @@ ActiveRecord::Schema.define(version: 2021_11_02_033753) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tbl_addrs", force: :cascade do |t|
+    t.string "last_name"
+    t.string "first_name"
+    t.string "state"
+    t.string "city"
+    t.integer "zip"
+    t.string "phone"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
